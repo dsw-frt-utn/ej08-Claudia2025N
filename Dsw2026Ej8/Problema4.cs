43afcb8df20b51954b44ad155850616dea3d572a
Claudia Olea <claudiaolea6@gmail.com>
2026-05-03 12:24:39 -0300

    Problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+    { 
+        double suma = 0;
+        int cantidadNotasValidas = 0;
+
+        // .HasValue verifica si la variable no es null de forma segura, evitando excepciones
+        //.Value accede al valor real de la variable nullable, pero solo si HasValue es true, por eso se evalúan juntos
+        //.HasValue es true si la variable tiene un valor asignado (no es null)
+        if (nota1.HasValue && nota1.Value >= 0 && nota1.Value <= 10) 
+        {
+            suma += nota1.Value;
+            cantidadNotasValidas++;
+        }
+        if (nota2.HasValue && nota2.Value >= 0 && nota2.Value <= 10) 
+        {
+            suma += nota2.Value;
+            cantidadNotasValidas++;
+        }
+
+        if (nota3.HasValue && nota3.Value >= 0 && nota3.Value <= 10)
+        {
+            suma += nota3.Value;
+            cantidadNotasValidas++;
+        }
+
+        
+        if (cantidadNotasValidas == 0)
+        {
+            return 0; 
+        }
+
+        
+        return suma / cantidadNotasValidas;
+    }
+}
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -14,6 +14,19 @@
             //Console.WriteLine(etiqueta);
             //Product productPrueba= new Product("Teclado Mecánico");
             //Console.WriteLine(new Problema3().CompararCopias(10, productPrueba));
+
+            //Console.WriteLine(new Problema4().CalcularPromedio(8, null, 9)); // Debería imprimir 8.5
+
+
+            //Problema5 problema5 = new Problema5();
+
+            // Probamos con la hija Retail (sin descuento)
+            //RetailSale ventaNormal = new RetailSale { ImporteBase = 1000m };
+            //Console.WriteLine(problema5.ObtenerImporteFinal(ventaNormal)); // Va a imprimir 1000
+
+            // Probamos con la hija Wholesale (con 10% de descuento)
+            //WholesaleSale ventaMayorista = new WholesaleSale { ImporteBase = 1000m };
+            //Console.WriteLine(problema5.ObtenerImporteFinal(ventaMayorista)); // Va a imprimir 900
         }
     }
 }
