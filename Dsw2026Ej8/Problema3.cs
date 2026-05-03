2b784a15b26cf7fd84715190f00a52a578d0f8a6
Claudia Olea <claudiaolea6@gmail.com>
2026-05-03 11:47:08 -0300

    Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorCopia = originalValue;
+            valorCopia ++;
+            Product copiaProducto = product;
+            
+            copiaProducto.CambiarDescripcion("Teclado color blanco con detalles en negro");
+            return $"{originalValue}-{valorCopia}-{product.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        
+        public string Description { get; private set; } 
+
+        public Product(string initialDescription)
+        {
+            Description = initialDescription;
+        }
+
+        
+        public void CambiarDescripcion(string nuevaDescripcion)
+        {
+            Description = nuevaDescripcion;
+        }
+    }
+}
