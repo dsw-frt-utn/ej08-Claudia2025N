7249cb3b7379c38b32e60ec1088073be89774be9
Claudia Olea <claudiaolea6@gmail.com>
2026-05-04 21:29:25 -0300

    Problema 5

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        
+        public decimal ImporteBase { get; set; }
+
+        // Método virtual 
+        public virtual decimal CalculateTotal()
+        {
+            return ImporteBase;
+        }
+    }
+
+    
+    public class RetailSale : Sale 
+    {
+       
+        public override decimal CalculateTotal()
+        {
+            return ImporteBase;
+        }
+    }
+
+    public class WholesaleSale : Sale 
+    {
+        // Redefinimos el método, aplicamos un 10% de descuento
+        public override decimal CalculateTotal()
+        {
+            // Multiplicar por 0.90m es lo mismo que restar el 10%
+            return ImporteBase * 0.90m;
+        }
+    }
+
+ 
+   
+}
